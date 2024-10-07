6633f4bd962eaacbc23b78c46115077c738baa29
Stepa <159813917+steepikk@users.noreply.github.com>
2024-10-07 16:49:11 +0300

    Add files via upload

---
+++ lab2/web_server/src/main/java/ru/itmo/web/AreaResult.java
@@ -0,0 +1,42 @@
+package ru.itmo.web;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class AreaResult {
+    private double x;
+    private double y;
+    private double r;
+    private boolean isInside;
+    private String currentTime;
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
+
+    public AreaResult(double x, double y, double r, boolean isInside) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.isInside = isInside;
+        this.currentTime = LocalDateTime.now().format(formatter);
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getR() {
+        return r;
+    }
+
+    public boolean getIsInside() {
+        return isInside;
+    }
+
+    public String getCurrentTime() {
+        return currentTime;
+    }
+}
